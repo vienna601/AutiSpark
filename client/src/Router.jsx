493fdceb9471c2e3782e3e7be52676eb7628e7d9
Angelaho1128 <hocheukwing1128@gmail.com>
2025-10-04 18:54:44 -0400

    add logo icon

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -4,12 +4,13 @@ import SignupPage from "@/features/auth/pages/SignupPage";
 import DashboardPage from "@/features/dashboard/pages/DashboardPage";
 import PlacementPage from "./pages/PlacementPage";
 import ProtectedRoute from "@/features/auth/components/ProtectedRoute";
+import ReadingPage from "@/features/reading/pages/ReadingPage";
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<ReadingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route
           path="/placement"
