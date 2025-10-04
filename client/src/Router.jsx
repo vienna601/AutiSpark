2a05d5da7e83a169012476c1497a73fe8d7d77bb
ZayraOriel05 <zayra.oriel@gmail.com>
2025-10-04 18:03:46 -0400

    home page

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import SignupPage from "@/features/auth/pages/SignupPage";
+import HomePage from "@/pages/HomePage";
 // later: import DashboardPage, ReadingPage, etc.
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
       </Routes>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
 
+// ... rendering App
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
