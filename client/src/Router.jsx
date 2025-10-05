f3e8d6c7a391f738a30bcab4c5edc8d83dedca16
ZayraOriel05 <zayra.oriel@gmail.com>
2025-10-05 00:24:21 -0400

    "Update styles"

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -4,17 +4,20 @@ import SignupPage from "@/features/auth/pages/SignupPage";
 import DashboardPage from "@/features/dashboard/pages/DashboardPage";
 import PlacementPage from "./pages/PlacementPage";
 import ProtectedRoute from "@/features/auth/components/ProtectedRoute";
-import ReadingPage from "@/features/reading/pages/ReadingPage";
 import HomePage from "@/pages/HomePage";
 import SpeakingPage from "@/features/speaking/pages/SpeakingPage";
 import WritingPage from "@/features/writing/pages/WritingPage";
+import ReadingPage from "@/features/reading/pages/ReadingPage";
 // later: import DashboardPage, ReadingPage, etc.
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ReadingPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reading" element={<ReadingPage />} />
+        <Route path="/writing" element={<WritingPage />} />
+        <Route path="/speaking" element={<SpeakingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route
           path="/placement"
