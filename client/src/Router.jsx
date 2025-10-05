35a9cbed0bcbc1d11af0cd7340efdb311ee50c09
Angelaho1128 <hocheukwing1128@gmail.com>
2025-10-05 00:15:27 -0400

    update routes

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -17,6 +17,7 @@ export default function AppRouter() {
         <Route path="/" element={<HomePage />} />
         <Route path="/reading" element={<ReadingPage />} />
         <Route path="/writing" element={<WritingPage />} />
+        <Route path="/reading" element={<ReadingPage />} />
         <Route path="/speaking" element={<SpeakingPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route
