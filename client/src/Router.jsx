0c3082b1c7b56ead4b9896e31cd8a9f56d1b328f
ZayraOriel05 <zayra.oriel@gmail.com>
2025-10-05 00:24:21 -0400

    "Update styles"

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -5,16 +5,20 @@ import HomePage from "@/pages/HomePage";
 import SpeakingPage from "@/features/speaking/pages/SpeakingPage";
 import WritingPage from "@/features/writing/pages/WritingPage";
 import ReadingPage from "@/features/reading/pages/ReadingPage";
+<<<<<<< HEAD
 import CongratsPage from "@/pages/CongratsPage";
 import LoginPage from "@/features/auth/pages/LoginPage";
 import SignupPage from "@/features/auth/pages/SignupPage";
 import DashboardPage from "@/pages/DashboardPage";
+=======
+>>>>>>> f3e8d6c ("Update styles")
 // later: import DashboardPage, ReadingPage, etc.
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
+<<<<<<< HEAD
         <Route path="/" element={<LoginPage />} />
         <Route path="/placement" element={<PlacementPage />} />
         <Route path="/home" element={<HomePage />} />
@@ -22,6 +26,12 @@ export default function AppRouter() {
         <Route path="/writing" element={<WritingPage />} />
         <Route path="/speaking" element={<SpeakingPage />} />
         <Route path="/congrats" element={<CongratsPage />} />
+=======
+        <Route path="/" element={<HomePage />} />
+        <Route path="/reading" element={<ReadingPage />} />
+        <Route path="/writing" element={<WritingPage />} />
+        <Route path="/speaking" element={<SpeakingPage />} />
+>>>>>>> f3e8d6c ("Update styles")
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
         <Route
