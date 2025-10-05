9ab626a655edc7957e6d6537b4793a997b0c0b3a
Angelaho1128 <hocheukwing1128@gmail.com>
2025-10-05 03:30:09 -0400

    added sign up page

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "@/features/auth/pages/LoginPage";
-import SignupPage from "@/features/auth/pages/SignupPage";
 import DashboardPage from "@/features/dashboard/pages/DashboardPage";
 import PlacementPage from "./pages/PlacementPage";
 import ProtectedRoute from "@/features/auth/components/ProtectedRoute";
@@ -9,20 +7,22 @@ import SpeakingPage from "@/features/speaking/pages/SpeakingPage";
 import WritingPage from "@/features/writing/pages/WritingPage";
 import ReadingPage from "@/features/reading/pages/ReadingPage";
 import CongratsPage from "@/pages/CongratsPage";
+import LoginPage from "@/features/auth/pages/LoginPage";
+import SignupPage from "@/features/auth/pages/SignupPage";
 // later: import DashboardPage, ReadingPage, etc.
 
 export default function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/placement" element={<PlacementPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/reading" element={<ReadingPage />} />
         <Route path="/writing" element={<WritingPage />} />
         <Route path="/speaking" element={<SpeakingPage />} />
         <Route path="/congrats" element={<CongratsPage />} />
-        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/" element={<SignupPage />} />
         <Route
           path="/placement"
           element={
