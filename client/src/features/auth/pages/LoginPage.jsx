58f09cbd17da1c0ddbbcb7151d956b791b36389f
Angelaho1128 <hocheukwing1128@gmail.com>
2025-10-18 18:06:45 -0400

    fix login page styling

--- client/src/features/auth/pages/LoginPage.jsx
+++ client/src/features/auth/pages/LoginPage.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 import "../../../styles/LoginPage.css";
-import logoIcon from "../../../assets/icon.png";
+import logoIcon from "../../../assets/logo-icon.png";
 import Button from "@/components/Button";
 
 const LoginPage = () => {
